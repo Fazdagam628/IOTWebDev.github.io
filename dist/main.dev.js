605caa47b5818a3b1556c8149e53d8ff86773828
Fazdagam628 <fauzanmasud628@gmail.com>
2025-04-10 11:41:13 +0700

    add file

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,15 +1,20 @@
+"use strict";
+
 function setLed1() {
   led1.style.background = "red";
   led1Image.src = "./assets/led-on.png";
 }
+
 function setLed2() {
   led2.style.background = "red";
   led2Image.src = "./assets/led-on.png";
 }
+
 function setLed3() {
   led3.style.background = "red";
   led3Image.src = "./assets/led-on.png";
 }
+
 function setLed4() {
   led4.style.background = "red";
   led4Image.src = "./assets/led-on.png";
