412ba9f1a99709f98125abb741d2cb52e17d6fb1
Fazdagam628 <fauzanmasud628@gmail.com>
2025-04-18 18:34:33 +0700

    add

--- main.js
+++ main.js
@@ -1,16 +1,96 @@
+const endpoint = "http://192.168.18.30";
+
+function getLed1() {
+  fetch(endpoint + "/led1", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led1.style.background = "blue";
+        led1Image.src = "./assets/led-on.png";
+      } else {
+        led1.style.background = "#579fff";
+        led1Image.src = "./assets/led-off.png";
+      }
+    });
+}
+function getLed2() {
+  fetch(endpoint + "/led2", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led2.style.background = "blue";
+        led2Image.src = "./assets/led-on.png";
+      } else {
+        led2.style.background = "#579fff";
+        led2Image.src = "./assets/led-off.png";
+      }
+    });
+}
+function getLed3() {
+  fetch(endpoint + "/led3", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led3.style.background = "blue";
+        led3Image.src = "./assets/led-on.png";
+      } else {
+        led3.style.background = "#579fff";
+        led3Image.src = "./assets/led-off.png";
+      }
+    });
+}
+function getLed4() {
+  fetch(endpoint + "/led4", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led4.style.background = "blue";
+        led4Image.src = "./assets/led-on.png";
+      } else {
+        led4.style.background = "#579fff";
+        led4Image.src = "./assets/led-off.png";
+      }
+    });
+}
+
 function setLed1() {
-  led1.style.background = "red";
-  led1Image.src = "./assets/led-on.png";
+  fetch(endpoint + "/led1", {
+    method: "POST",
+  })
+    .then((respond) => respond.text())
+    .then(() => location.reload());
 }
 function setLed2() {
-  led2.style.background = "red";
-  led2Image.src = "./assets/led-on.png";
+  fetch(endpoint + "/led2", {
+    method: "POST",
+  })
+    .then((respond) => respond.text())
+    .then(() => location.reload());
 }
 function setLed3() {
-  led3.style.background = "red";
-  led3Image.src = "./assets/led-on.png";
+  fetch(endpoint + "/led3", {
+    method: "POST",
+  })
+    .then((respond) => respond.text())
+    .then(() => location.reload());
 }
 function setLed4() {
-  led4.style.background = "red";
-  led4Image.src = "./assets/led-on.png";
+  fetch(endpoint + "/led4", {
+    method: "POST",
+  })
+    .then((respond) => respond.text())
+    .then(() => location.reload());
 }
+
+getLed1();
+getLed2();
+getLed3();
+getLed4();
